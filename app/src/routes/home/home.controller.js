85a076f76a68e8b1d5b1d1422746a2f7402e6b07
racodin <skkim6.dev@gmail.com>
2022-09-01 23:29:36 +0900

    User 모델 구현

--- app/src/routes/home/home.controller.js
+++ app/src/routes/home/home.controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -13,23 +13,28 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const password = req.body.password;
+    const user = new User(req.body);
+    const response = user.login();
+    console.log(response);
+    // return res.json(response);
 
-    const users = UserStorage.getUsers("id", "password");
-    const response = {};
+    // const id = req.body.id;
+    // const password = req.body.password;
 
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.password[idx] === password) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
+    // const users = UserStorage.getUsers("id", "password");
+    // const response = {};
 
-    response.success = false;
-    response.msg = "로그인에 실패하였슶니다.";
-    return res.json(response);
+    // if (users.id.includes(id)) {
+    //   const idx = users.id.indexOf(id);
+    //   if (users.password[idx] === password) {
+    //     response.success = true;
+    //     return res.json(response);
+    //   }
+    // }
+
+    // response.success = false;
+    // response.msg = "로그인에 실패하였슶니다.";
+    // return res.json(response);
   },
 };
 
