a57faf91321590cebf9e598dadae74df37b26621
racodin <skkim6.dev@gmail.com>
2022-09-01 19:59:11 +0900

    로그인 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,5 +10,13 @@ function login() {
     id: id.value,
     password: password.value,
   };
-  console.log(req);
+
+  //   console.log(req, JSON.stringify(req));
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
