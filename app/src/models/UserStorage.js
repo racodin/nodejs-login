7b1196a31cf0ad4084bc6f6938e47f5f329112e8
racodin <skkim6.dev@gmail.com>
2022-09-02 00:59:21 +0900

    fs모듈을 통한 데이터 분리

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,27 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  static #users = {
-    id: ["rome", "apple", "banana"],
-    password: ["1234", "1234", "1212"],
-    name: ["김선규", "이동률", "유민석"],
-  };
+  // static #users = {
+  //   id: ["rome", "apple", "banana"],
+  //   password: ["1234", "1234", "1212"],
+  //   name: ["김선규", "이동률", "유민석"],
+  // };
+
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users);
+    const userInfo = userKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -19,18 +32,17 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userKeys = Object.keys(users);
-    const userInfo = userKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
+    // const users = this.#users;
+    return fs
+      .readFile("./src/db/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
-  static save(userInfo) {
-    const users = this.#users;
+  static save() {
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
