caadf87ac40777d0a2d6dc8f3b5152ed8c9167f2
racodin <skkim6.dev@gmail.com>
2022-09-01 23:45:49 +0900

    로그인 화면 꾸미기 추가

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -5,6 +5,7 @@ const password = document.querySelector("#password");
 const loginButton = document.querySelector("#login-button");
 
 loginButton.addEventListener("click", login);
+
 function login() {
   const req = {
     id: id.value,
