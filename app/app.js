c19ecb5beb8bf08cd5b1c4b4ea1c5f9306cc0a56
racodin <skkim6.dev@gmail.com>
2022-09-01 19:40:38 +0900

    노드몬 모듈 추가

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
 "use strict";
 
+// modules
 const express = require("express");
 const app = express();
 
-// route
+// routes
 const home = require("./src/routes/home");
 
 // setting
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); // middleware
 
 module.exports = app;
