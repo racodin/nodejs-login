7f22c8231f7c769271992e936616771ccd65f7ce
racodin <skkim6.dev@gmail.com>
2022-09-02 15:00:47 +0900

    http 상태코드 추가 및 최적화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,7 +7,7 @@ class UserStorage {
 
   static getUserInfo(id) {
     return new Promise((resolve, reject) => {
-      const query = "SELECT * FROM usedrs WHERE id = ?;";
+      const query = "SELECT * FROM users WHERE id = ?;";
       db.query(query, [id], (err, data) => {
         if (err) reject(`${err}`);
         else resolve(data[0]);
